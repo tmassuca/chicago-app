a9185e647ee557c236dfaaf22acfcc96d9074a1b
Thiago Massucato <59482212+tmassuca@users.noreply.github.com>
2026-06-02 09:35:43 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,11 @@
-const CACHE = 'chicago-v1';
-const ASSETS = ['/', '/index.html'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+const CACHE = 'chicago-v2';
+const ASSETS = [
+  '/chicago-app/',
+  '/chicago-app/index.html',
+  '/chicago-app/manifest.json',
+  '/chicago-app/icon-192.png',
+  '/chicago-app/icon-512.png'
+];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
